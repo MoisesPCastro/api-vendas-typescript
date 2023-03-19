b689b2c91011db319e78b34edfe5d77adaebed3e
Moises Pires <mouchespires@gmail.com>
2023-03-19 11:53:04 -0300

    subir git

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -13,8 +13,8 @@ export default {
 
   defaults: {
     from: {
-      email: 'contato@aluiziodeveloper.cf',
-      name: 'Jorge Aluizio',
+      email: 'mouchespires@gmail.com',
+      name: 'Moises Pires',
     },
   },
 } as IMailConfig;
